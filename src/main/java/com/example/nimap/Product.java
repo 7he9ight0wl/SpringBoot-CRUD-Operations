4e08acd489959e9662c108e8c1c7e8b7a4f547fb
7he9ight0wl <mestrypradumana1999@gmail.com>
2024-03-27 20:29:08 +0530

    Initial commit

---
+++ src/main/java/com/example/nimap/Product.java
@@ -0,0 +1,35 @@
+package com.example.nimap;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+    public Product() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @ManyToOne
+    private Category category ;
+}
